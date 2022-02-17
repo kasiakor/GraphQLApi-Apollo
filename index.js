c29a4ae10a1eecacb244ab4abd8f4b7f562ef5a7
kasia.kordys <kasia.kordys@gmail.com>
2022-02-17 12:51:26 +0200

    fix error with grapghql@15

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const {ApolloServer}= require('apollo-server');
+const {ApolloServer, gql}= require('apollo-server');
 
-const server = new ApolloServer();
+//define object that will hold the schema for API
+//define what queries are allowe by the API
+//gql function - JavaScript template literal tag that parses GraphQL queries into an abstract syntax tree (AST). It is the recommended method for passing queries to Apollo Client
+const typeDefs = gql`
+type Query {
+    sessions:[Session]
+}
+type Session {
+    id: ID!,
+    title: String!,
+    description: String,
+    startsAt: String,
+    endsAt: String,
+    room: String,
+    day: String,
+    format:String,
+    track:String,
+    level:String
+}`
+
+const server = new ApolloServer({typeDefs});
 
 server
 .listen({port: process.env.PORT || 4000})
